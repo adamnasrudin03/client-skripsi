8f796415f2e1518a479d41853fcc9ccbe873c271
Adam Nasrudin <adamnasrudin858@gmail.com>
2022-04-03 15:22:46 +0700

    fix layout navbar

--- components/organisms/Navbar/index.tsx
+++ components/organisms/Navbar/index.tsx
@@ -8,16 +8,15 @@ export default function Navbar() {
       <nav className="navbar navbar-expand-lg navbar-light bg-light bg-white pt-lg-40 pb-lg-40 pt-30 pb-50">
         <div className="container-fluid">
           <a className="navbar-brand" href="/#">
-            <Image src="/icon/logo.svg" width={60} height={60} />
+            <Image src="/logo.png" width={85} height={85} />
           </a>
           <ToggleMenu />
           <div className="collapse navbar-collapse" id="navbarNav">
             <ul className="navbar-nav ms-auto text-lg gap-lg-0 gap-2">
               <Menu title="Home" active />
-              <Menu title="Games" href="/games" />
-              <Menu title="Reward" />
-              <Menu title="Discover" />
-              <Menu title="Global Rank" />
+              <Menu title="Syarat" href="#syarat" />
+              <Menu title="Tema" href="#tema-skripsi" />
+              <Menu title="Pengajuan" href="#pengajuan" />
             </ul>
           </div>
         </div>
