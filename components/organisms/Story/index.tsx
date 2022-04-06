1363f3a9dc66d044bfe672e034f1d3f20a23cc3b
Adam Nasrudin <adamnasrudin858@gmail.com>
2022-04-06 22:40:12 +0700

    add animation & adjust wording

--- components/organisms/Story/index.tsx
+++ components/organisms/Story/index.tsx
@@ -1,10 +1,25 @@
+import Lottie from 'lottie-react';
+import Blogging from '../../atoms/Animations/Blogging.json';
+
 export default function Story() {
   return (
     <section className="story pt-50 pb-50" id="pengajuan">
       <div className="container-xxl container-fluid">
         <div className="row align-items-center px-lg-5 mx-auto gap-lg-0 gap-4">
           <div className="col-lg-7 col-12 d-lg-flex d-none justify-content-lg-end pe-lg-60" data-aos="zoom-in">
-            <img src="/img/ilustrations/Vision.png" width="612" height="452" className="img-fluid" alt="" />
+
+            <Lottie
+              animationData={Blogging}
+              loop
+              autoPlay
+              className="img-fluid"
+              style={{
+                margin: 'auto',
+                height: 452,
+                width: 612,
+              }}
+            />
+            {/* <img src="/img/ilustrations/Vision.png" width="612" height="452" className="img-fluid" alt="" /> */}
           </div>
           <div className="col-lg-5 col-12 ps-lg-60">
             <div className="">
@@ -15,14 +30,13 @@ export default function Story() {
                 Proposal Skripsi ?
               </h2>
               <p className="text-lg color-palette-1 mb-30">
-                Kami menyediakan jutaan cara untuk
+                Jika sudah memenuhi syarat, tunggu apa
                 <br
                   className="d-sm-block d-none"
                 />
-                membantu players menjadi
+                lagi. kirim pengajuan proposal skrispsi sekarang.
                 <br className="d-sm-block d-none" />
                 {' '}
-                pemenang sejati
 
               </p>
               <div className="d-md-block d-flex flex-column w-100">
