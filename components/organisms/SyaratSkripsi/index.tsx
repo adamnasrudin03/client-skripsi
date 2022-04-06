f207b4f68d5b3566ce0de31631d642fd37e7f689
Adam Nasrudin <adamnasrudin858@gmail.com>
2022-04-06 16:44:52 +0700

    fix layout syarat skripsi

--- components/organisms/SyaratSkripsi/index.tsx
+++ components/organisms/SyaratSkripsi/index.tsx
@@ -13,11 +13,8 @@ export default function SyaratSkripsi() {
         <div className="row gap-lg-0 gap-4" data-aos="fade-up">
           <StepItem icon="step1" title="1. Mengisi KRS Skripsi" desc1="Mahasiswa wajib mengisi KRS " desc2="skripsi pada semester yang berjalan." />
           <StepItem icon="step2" title="2. Memenuhi syarat SKS" desc1="Mahasiswa telah menempuh minimal 120 / 130 SKS " desc2="saat mengambil skripsi." />
-          <StepItem icon="step3" title="3. Tidak ada nilai D dan E" desc1="Mahasiswa tidak boleh mendapatkan nilai D dan E" desc2="improve permainan kamu" />
+          <StepItem icon="step3" title="3. Tidak ada nilai D dan E" desc1="Jika terdapat nilai D dan E" desc2="Mahasiswa wajib mengulang mata kuliah tersebut" />
           <div className="mt-5">  </div>
-          <StepItem icon="step1" title="4. Start" desc1="Pilih salah satu game" desc2="yang ingin kamu top up" />
-          <StepItem icon="step2" title="5. Fill Up" desc1="Top up sesuai dengan" desc2="nominal yang sudah tersedia" />
-          <StepItem icon="step3" title="6. Be a Winner" desc1="Siap digunakan untuk" desc2="improve permainan kamu" />
         </div>
       </div>
     </section>
