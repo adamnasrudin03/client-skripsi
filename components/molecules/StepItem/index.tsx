6098cf2b1ec53af3a084a58160a0a1359c705913
Adam Nasrudin <adamnasrudin858@gmail.com>
2022-04-06 16:46:05 +0700

    fix layout component

--- components/molecules/StepItem/index.tsx
+++ components/molecules/StepItem/index.tsx
@@ -1,5 +1,7 @@
 export interface StepItemProps {
-    icon: 'step1' | 'step2' | 'step3'
+    icon: 'step1' | 'step2' | 'step3' | 'website-design'
+    | 'mobile' | 'iot' | 'dashboard' | 'integration' | 'shipping'
+    | 'logo-design' | 'disconnect'
     title: string;
     desc1: string;
     desc2: string;
@@ -10,7 +12,7 @@ export default function StepItem(props: StepItemProps) {
   } = props;
   return (
     <div className="col-lg-4">
-      <div className="card feature-card border-0">
+      <div className="card feature-card border-0" style={{ minHeight: '350px' }}>
         <img src={`/icon/${icon}.svg`} alt="icon step" className="mb-30" width={80} height={80} />
         <p className="fw-semibold text-2xl mb-2 color-palette-1">{title}</p>
         <p className="text-lg color-palette-1 mb-0">
