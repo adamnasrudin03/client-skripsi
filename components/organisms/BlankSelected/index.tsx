95880f2aeded110f3200d62c62b30f4dbb872fa9
Adam Nasrudin <adamnasrudin858@gmail.com>
2022-04-19 20:45:16 +0700

    fix wording

--- components/organisms/BlankSelected/index.tsx
+++ components/organisms/BlankSelected/index.tsx
@@ -20,13 +20,14 @@ export default function BlankSelected() {
           />
         </div>
         <div className="pt-10 pb-md-50 pb-150">
-          <h2 className="text-4xl fw-bold text-center color-palette-1 mb-10">Wajib Memilih Tahun Ajaran.</h2>
+          <h2 className="text-4xl fw-bold text-center color-palette-1 mb-10">Oops! Not Selected..</h2>
           <p className="text-lg text-center color-palette-1 m-0">
-            Harap memilih tahun ajaran yang dimaksud, agar dapat melanjutkan pengajuan proposal skripsi.
+            Harap memilih tahun ajaran yang dimaksud,
+            {' '}
             <br
               className="d-sm-block d-none"
             />
-            ~Terimakasih~
+            agar dapat melanjutkan pengajuan proposal skripsi.
 
           </p>
         </div>
