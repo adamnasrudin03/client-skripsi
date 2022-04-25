24135d30de9b9c7440686c031e866f73a528f52a
Adam Nasrudin <adamnasrudin858@gmail.com>
2022-04-25 21:44:26 +0700

    type data pengajuan

--- services/data-types/index.ts
+++ services/data-types/index.ts
@@ -1,3 +1,4 @@
+/* eslint-disable camelcase */
 export interface AjaranTypes {
     _id: string;
     startYear: string;
@@ -5,4 +6,40 @@ export interface AjaranTypes {
     semester: string;
     startAt: string;
     endAt: string;
+}
+export interface RequestPengajuanType {
+    npm: string;
+    nama: string;
+    email: string;
+    lanjutan: boolean;
+    semester: string;
+    no_wa: string;
+    judul_skripsi: string;
+    tema_skripsi: string;
+    file_proposal: string;
+    file_rekap_nilai: string;
+    dosen_sebelum: string;
+    ajaran_id: string;
+}
+
+export interface ResponsePengajuanTypes {
+    message?: string;
+    data: {
+        _id: string;
+        npm: string;
+        nama: string;
+        email: string;
+        lanjutan: boolean;
+        semester: string;
+        no_wa: string;
+        judul_skripsi: string;
+        tema_skripsi: string;
+        status: string;
+        file_proposal: string;
+        file_rekap_nilai: string;
+        dosen_sebelum: string;
+        ajaran_id: string;
+        created_at: string;
+        updated_at: string;
+    }
 }
