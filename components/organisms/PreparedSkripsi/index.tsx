a052527aa0d3a154db0a9f21cb8920f449f77d36
Adam Nasrudin <adamnasrudin858@gmail.com>
2022-05-29 07:56:42 +0700

    fix wording

--- components/organisms/PreparedSkripsi/index.tsx
+++ components/organisms/PreparedSkripsi/index.tsx
@@ -13,7 +13,7 @@ export default function PreparedSkripsi() {
         <div className="row gap-lg-0 gap-4" data-aos="fade-up">
           <StepItem icon="step2" title="1. Tema Skripsi" desc1="Mahasiswa wajib memilih " desc2="tema skripsi yang telah disediakan." />
           <StepItem icon="step3" title="2. Dokumen Proposal Skripsi" desc1="Mahasiswa wajib meyiapkan " desc2="Dokumen Proposal Skripsi dengan format .doc" />
-          <StepItem icon="step1" title="3. Dokumen Rekap Nilai Mahasiswa" desc1="Mahasiswa wajib menyiapkan " desc2="Dokumen Rekap Nilai sampai semester saat ini, dengan format .pdf" />
+          <StepItem icon="step1" title="3. Dokumen Rekap Nilai Mahasiswa" desc1="Mahasiswa wajib menyiapkan " desc2="Dokumen Rekap Nilai sampai semester yang berjalan, dengan format .pdf" />
           <div className="mt-5">  </div>
         </div>
       </div>
